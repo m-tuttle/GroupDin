ade53e880abe582b99c5a09407c23623b099a722
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-02-17 21:31:18 -0600

    added js comments

--- assets/JavaScript/logic.js
+++ assets/JavaScript/logic.js
@@ -2,16 +2,20 @@ $(document).ready(function () {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
 
+    //click handler for adding restaurant
     $("#add-restaurant").on("click", function (event) {
 
+        //prevents page reload
         event.preventDefault();
 
+        //search variables
         var restaurant = $("#text-box").val().trim();
         var result = restaurant.replace(" ", "%20");
         var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=chicago&entity_type=city&count=1&q=" + result;
 
         console.log(queryURL);
 
+        //calls to zomato API
         $.ajax({
             url: queryURL,
             headers: {
@@ -19,11 +23,16 @@ $(document).ready(function () {
             },
             method: "GET"
         }).then(function (response) {
+
             console.log(response);
+
+            //variables to call specific restaurant by ID
             var businessID = response.restaurants[0].restaurant.R.res_id;
             var businessURL = "https://developers.zomato.com/api/v2.1/restaurant?res_id=" + businessID;
 
             console.log(businessID);
+
+            //ajax call to zomato API with restaurant ID
             $.ajax({
                 url: businessURL,
                 headers: {
@@ -31,6 +40,7 @@ $(document).ready(function () {
                 },
                 method: "GET"
             }).then(function (response) {
+
                 console.log(response);
             });
 
